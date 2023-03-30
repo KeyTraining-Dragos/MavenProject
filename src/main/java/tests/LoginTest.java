340f4207f42eb03f50aba9a62c115cee624f991e
Dragos Tanta <dragos.tanta@gmail.com>
2023-03-30 21:39:54 +0300

    curs12

---
+++ src/main/java/tests/LoginTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
+import pageObjects.MenuPage;
+import utils.BaseTest;
+
+public class LoginTest  extends BaseTest {
+	
+	
+	@Test
+	public void loginTest() {
+		
+		MenuPage menu =  new MenuPage(driver);
+		menu.navigateTo(menu.contactsLink);
+		menu.navigateTo(menu.loginLink);
+		
+		LoginPage login = new LoginPage(driver);
+		login.loginInApp("TestUser", "12345@67890");
+		
+		assertTrue(login.successMsgisDisplayed());
+	}
+	
+
+}
